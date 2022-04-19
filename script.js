f722a9062a232edd0d2e4144d666e421834fb080
paulovictor1997 <paulovictornunes97@gmail.com>
2022-04-18 21:46:28 -0300

    Final Version

---
+++ script.js
@@ -0,0 +1,18 @@
+let form = document.querySelector('.form');
+let email = document.getElementById('email');
+
+form.addEventListener('submit',e =>{
+    e.preventDefault();
+    let emailValue = email.value;
+
+    if(!validateEmail(emailValue)){
+       document.querySelector('.erroText').style.display = 'block';
+    }else{
+       document.querySelector('.erroText').style.display = 'none';
+    }
+});
+
+function validateEmail(email) {
+    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+}
